027b6bf002e46fb49d311fe3ff0f1fd09fc1a474
Krishan Rana <krishanrana100@gmail.com>
2021-01-01 16:01:25 +1000

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  let x = random(width);
+  
+  //let x = map(noise(frameCount*0.01), 0, 1, 0, width);
+  
+  //let x = map(sin(frameCount*0.01), -1, 1, 0, width);
+  
+  ellipse(x,200,24,24);
+  
+}
