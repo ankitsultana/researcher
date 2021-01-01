e8ffbea3885b59dc874ac0063bf9faa053b46fa9
Krishan Rana <krishanrana100@gmail.com>
2021-01-01 16:57:18 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var canvas;
 function setup() {
   canvas = createCanvas(400, 400);
   canvas.position(0,50);
-  //canvas.style('z-index', '-1');
+  canvas.style('z-index', '-1');
 }
 
 function draw() {
