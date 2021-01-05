b8b81119f4e2530e0f9f94777764350ee4c3b585
Krishan Rana <krishanrana100@gmail.com>
2021-01-05 20:11:23 +1000

    Update sketch_qlearn.js

--- sketch_qlearn.js
+++ sketch_qlearn.js
@@ -7,7 +7,7 @@ var Q_array = [];
 
 function setup() {
   canvas = createCanvas( windowWidth/2, windowWidth/2+20);
-  canvas.position(200,200);
+  canvas.position(windowWidth/2, windowHeight/2);
   canvas.style('z-index', '-1');
   reset();
 }
